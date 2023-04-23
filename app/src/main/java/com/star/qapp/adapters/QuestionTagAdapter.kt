074c22ca3319dd606d892e6b1f393e78f9b13679
junrdev <juniorprogrammer09@gmail.com>
2023-04-23 13:59:09 +0300

    tags adapter class

---
+++ app/src/main/java/com/star/qapp/adapters/QuestionTagAdapter.kt
@@ -0,0 +1,36 @@
+package com.star.qapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.star.qapp.R
+
+class QuestionTagAdapter(val tags : List<String>) : RecyclerView.Adapter<QuestionTagAdapter.CustViewHolder>() {
+
+
+    class CustViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val content: TextView
+
+        init {
+            content = itemView.findViewById(R.id.tagtext)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustViewHolder {
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.tags, parent, false)
+        return CustViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return tags.size
+    }
+
+    override fun onBindViewHolder(holder: CustViewHolder, position: Int) {
+//        set the fields
+        holder.content.text = tags[position]
+    }
+}
